17cb8b9680157ce64d7de711438c52f6fbfebfa1
Christopher <2193014b@student.gla.ac.uk>
2017-11-23 21:26:07 +0000

    Google maps API embedded in template - NOT WORKING

--- buddyapp/views.py
+++ buddyapp/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Bike Buddy says hello world!")
+    context = RequestContext(request)
+    return render_to_response('buddyapp/base.html', context)
