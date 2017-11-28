124fa36b535e8ea70542cf605e2d22ea9615cb13
Christopher <2193014b@student.gla.ac.uk>
2017-11-28 12:20:09 +0000

    Altered form layout

--- buddyapp/forms.py
+++ buddyapp/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from buddyapp.models import Route
 
 class UploadRouteForm(forms.Form):
-    name = forms.CharField(max_length = 50, help_text="Name: ")
-    description = forms.CharField(max_length = 300, help_text = "Description: ")
+    name = forms.CharField(max_length = 50, help_text="Name: ", required = True)
+    description = forms.CharField(max_length =140, help_text = "Description: ", required = False)
     route = forms.FileField()
