222318e36230b0f0eca948a22812ec12c0aaef1d
Christopher <2193014b@student.gla.ac.uk>
2017-11-30 14:10:07 +0000

    Regitsration now works and landing page changed to login page

--- bikebuddy/urls.py
+++ bikebuddy/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^buddyapp/', include('buddyapp.urls')),
-	url(r'^$', views.index, name = 'index'),
+	url(r'^$', views.user_login, name = 'login'),
     url(r'^home/$', views.index, name='index'),
     url(r'^my_routes/$', views.my_routes, name='my_routes'),
     url(r'^add_route/$', views.add_route, name='add_route'),
