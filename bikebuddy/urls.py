9bde8bd2468a435121abb44b87188a383864ce15
Christopher <2193014b@student.gla.ac.uk>
2017-11-30 13:56:52 +0000

    Fixed admin

--- bikebuddy/urls.py
+++ bikebuddy/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from buddyapp import views
-
+from django.contrib import admin
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -19,8 +19,7 @@ urlpatterns = patterns('',
     url(r'^home/$', views.index, name='index'),
     url(r'^my_routes/$', views.my_routes, name='my_routes'),
     url(r'^add_route/$', views.add_route, name='add_route'),
-
-   # url(r'^admin/', admin.site.urls, name = 'admin'),
+    url(r'^admin/', include(admin.site.urls)),
     #url(r'^route/(?P<route_name_slug>[\w\-]+)/$', views.show_route, name='show_route'),
     #url(r'^my_routes/$', views.my_routes, name='my_routes'),
     #url(r'^home/accounts/password_change/$', auth_views.password_change, name='password_change'),
